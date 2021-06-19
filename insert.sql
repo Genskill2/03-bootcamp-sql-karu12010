6a427cc844eb6efcaf0bf56c092439f14a2e9900
karu12010 <84575351+karu12010@users.noreply.github.com>
2021-06-19 23:47:48 +0530

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -8,7 +8,7 @@ insert into subjects(name) values ('C');
 insert into subjects(name) values ('UNIX');
 insert into subjects(name) values ('Technology');
 insert into subjects(name) values ('Go');
-insert into subjects(name) values ('ScienCe Fiction');
+insert into subjects(name) values ('Science Fiction');
 insert into subjects(name) values ('Productivity');
 insert into subjects(name) values ('Psychology');
 insert into subjects(name) values ('Politics');
